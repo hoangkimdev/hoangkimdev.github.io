3908beaaf900159ae3f3ccef39f07fdbb69687d6
hoangkim1711 <kim.dinhhoang1711@hcmut.edu.vn>
2020-11-01 08:44:01 +0700

    update colorful + delete contact form

--- js/script.js
+++ js/script.js
@@ -6,3 +6,31 @@ function openNav() {
         x.className = "topnav";
     }
 }
+function closeNav(){
+    var x = document.getElementById("myTopnav");
+    x.className = "topnav";
+}
+function openAbo() {
+    var x = document.getElementById("body");
+    x.className = "body abo";
+}
+function openEdu() {
+    var x = document.getElementById("body");
+    x.className = "body edu";
+}
+function openExp() {
+    var x = document.getElementById("body");
+    x.className = "body exp";
+}
+function openPro() {
+    var x = document.getElementById("body");
+    x.className = "body pro";
+}
+function openSki() {
+    var x = document.getElementById("body");
+    x.className = "body ski";
+}
+function openCon() {
+    var x = document.getElementById("body");
+    x.className = "body con";
+}
